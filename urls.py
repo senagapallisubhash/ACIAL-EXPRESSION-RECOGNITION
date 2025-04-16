b1d6758a579e21f31fee7daa24215c72d00bc164
Subhash <senagapallisubhash@gmail.com>
2025-04-16 15:18:06 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,22 @@
-from django.urls import path
+"""Facial URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
-urlpatterns = [path("index.html", views.Index, name="Index"),
-	       path("User.html", views.User, name="User"),
-	       path("Rating", views.Rating, name="Rating"),
-	       path("Admin.html", views.Admin, name="Admin"),
-	       path("AdminLogin", views.AdminLogin, name="AdminLogin"),
-	       path("ViewRating", views.ViewRating, name="ViewRating"),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('FacialApp.urls')),
 ]
